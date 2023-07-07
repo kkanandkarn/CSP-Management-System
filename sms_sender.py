a8386ddad2063a8c0cdb477cb6c2d719e6cb1be7
Anand Kumar Karn <123385713+kkanandkarn@users.noreply.github.com>
2023-07-07 23:23:31 +0530

    Update sms_sender.py

--- sms_sender.py
+++ sms_sender.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 # Your Twilio account SID and auth token
-account_sid = 'ACcfa7ed97949bb58abe841bfea67f1bc6'
-auth_token = '3cff29f4b95cb7155ce10594a6c46be0'
+account_sid = 'twilio account_sid'
+auth_token = 'twilio auth_token'
 
 # Create a Twilio client
 client = Client(account_sid, auth_token)
